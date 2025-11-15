b2b6d8f324a621e6497b38ac91eee47b14608e99
senolem <37041587+senolem@users.noreply.github.com>
2025-11-16 00:18:57 +0100

    added Outer Ra'Kaznar [U2] - Sortie  redirections

---
+++ Outer Ra'Kaznar [U2] - Sortie/settings.lua
@@ -0,0 +1,100 @@
+-- Add this to your settings.lua file inside <Ashita Folder>/config/addons/boussole/<YourCharacter_XXXXXX>
+-- Then, add the content of the custom_maps folder inside <Ashita Folder>/config/addons/boussole/custom_maps
+
+-- Outer Ra'Kaznar [U2] - Sortie - Upper floors
+settings["mapRedirects"]["133_1"] = T {};
+settings["mapRedirects"]["133_1"]["offsetY"] = 32;
+settings["mapRedirects"]["133_1"]["offsetX"] = 92;
+settings["mapRedirects"]["133_1"]["targetZone"] = 133;
+settings["mapRedirects"]["133_1"]["targetFloor"] = 4;
+
+settings["mapRedirects"]["133_2"] = T {};
+settings["mapRedirects"]["133_2"]["offsetY"] = 32;
+settings["mapRedirects"]["133_2"]["offsetX"] = 92;
+settings["mapRedirects"]["133_2"]["targetZone"] = 133;
+settings["mapRedirects"]["133_2"]["targetFloor"] = 4;
+
+settings["mapRedirects"]["133_3"] = T {};
+settings["mapRedirects"]["133_3"]["offsetY"] = 32;
+settings["mapRedirects"]["133_3"]["offsetX"] = 92;
+settings["mapRedirects"]["133_3"]["targetZone"] = 133;
+settings["mapRedirects"]["133_3"]["targetFloor"] = 4;
+
+settings["mapRedirects"]["133_4"] = T {};
+settings["mapRedirects"]["133_4"]["offsetY"] = 32;
+settings["mapRedirects"]["133_4"]["offsetX"] = 92;
+settings["mapRedirects"]["133_4"]["targetZone"] = 133;
+settings["mapRedirects"]["133_4"]["targetFloor"] = 4;
+
+settings["mapRedirects"]["133_5"] = T {};
+settings["mapRedirects"]["133_5"]["offsetY"] = 32;
+settings["mapRedirects"]["133_5"]["offsetX"] = 92;
+settings["mapRedirects"]["133_5"]["targetZone"] = 133;
+settings["mapRedirects"]["133_5"]["targetFloor"] = 4;
+
+settings["mapRedirects"]["133_6"] = T {};
+settings["mapRedirects"]["133_6"]["offsetY"] = 32;
+settings["mapRedirects"]["133_6"]["offsetX"] = 92;
+settings["mapRedirects"]["133_6"]["targetZone"] = 133;
+settings["mapRedirects"]["133_6"]["targetFloor"] = 4;
+
+settings["mapRedirects"]["133_7"] = T {};
+settings["mapRedirects"]["133_7"]["offsetY"] = 32;
+settings["mapRedirects"]["133_7"]["offsetX"] = 92;
+settings["mapRedirects"]["133_7"]["targetZone"] = 133;
+settings["mapRedirects"]["133_7"]["targetFloor"] = 4;
+
+settings["mapRedirects"]["133_8"] = T {};
+settings["mapRedirects"]["133_8"]["offsetY"] = 32;
+settings["mapRedirects"]["133_8"]["offsetX"] = 92;
+settings["mapRedirects"]["133_8"]["targetZone"] = 133;
+settings["mapRedirects"]["133_8"]["targetFloor"] = 4;
+
+settings["mapRedirects"]["133_9"] = T {};
+settings["mapRedirects"]["133_9"]["offsetY"] = 32;
+settings["mapRedirects"]["133_9"]["offsetX"] = 92;
+settings["mapRedirects"]["133_9"]["targetZone"] = 133;
+settings["mapRedirects"]["133_9"]["targetFloor"] = 4;
+
+-- Outer Ra'Kaznar [U2] - Sortie - Basement floors
+settings["mapRedirects"]["133_10"] = T {};
+settings["mapRedirects"]["133_10"]["offsetY"] = 48;
+settings["mapRedirects"]["133_10"]["offsetX"] = 56;
+settings["mapRedirects"]["133_10"]["targetZone"] = 133;
+settings["mapRedirects"]["133_10"]["targetFloor"] = 10;
+
+settings["mapRedirects"]["133_11"] = T {};
+settings["mapRedirects"]["133_11"]["offsetY"] = 48;
+settings["mapRedirects"]["133_11"]["offsetX"] = 56;
+settings["mapRedirects"]["133_11"]["targetZone"] = 133;
+settings["mapRedirects"]["133_11"]["targetFloor"] = 10;
+
+settings["mapRedirects"]["133_12"] = T {};
+settings["mapRedirects"]["133_12"]["offsetY"] = 48;
+settings["mapRedirects"]["133_12"]["offsetX"] = 56;
+settings["mapRedirects"]["133_12"]["targetZone"] = 133;
+settings["mapRedirects"]["133_12"]["targetFloor"] = 10;
+
+settings["mapRedirects"]["133_13"] = T {};
+settings["mapRedirects"]["133_13"]["offsetY"] = 48;
+settings["mapRedirects"]["133_13"]["offsetX"] = 56;
+settings["mapRedirects"]["133_13"]["targetZone"] = 133;
+settings["mapRedirects"]["133_13"]["targetFloor"] = 10;
+
+settings["mapRedirects"]["133_14"] = T {};
+settings["mapRedirects"]["133_14"]["offsetY"] = 48;
+settings["mapRedirects"]["133_14"]["offsetX"] = 56;
+settings["mapRedirects"]["133_14"]["targetZone"] = 133;
+settings["mapRedirects"]["133_14"]["targetFloor"] = 10;
+
+settings["mapRedirects"]["133_15"] = T {};
+settings["mapRedirects"]["133_15"]["offsetY"] = 48;
+settings["mapRedirects"]["133_15"]["offsetX"] = 56;
+settings["mapRedirects"]["133_15"]["targetZone"] = 133;
+settings["mapRedirects"]["133_15"]["targetFloor"] = 10;
+
+settings["mapRedirects"]["133_16"] = T {};
+settings["mapRedirects"]["133_16"]["offsetY"] = 48;
+settings["mapRedirects"]["133_16"]["offsetX"] = 56;
+settings["mapRedirects"]["133_16"]["targetZone"] = 133;
+settings["mapRedirects"]["133_16"]["targetFloor"] = 10;
